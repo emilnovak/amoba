4f5decfd9e14879b5e2c23ce3667da6c6a890ec5
emilnovak <emil.tibor.novak@gmail.com>
2019-02-13 23:46:04 +0100

    the player can now place tiled on valid spaces

--- src/new.py
+++ src/new.py
@@ -22,13 +22,14 @@ emptyChar = ''
 playerChar = 'X'
 aiChar = 'O'
 
-gameField = [['X', '', ''], ['', '', ''], ['', '', '']]
+gameField = [['', '', ''], ['', '', ''], ['', '', '']]
 gameFieldAnchor = (window_width / 2 - tileSize, window_height / 2 - tileSize)
 
 prevDragPosition = (0, 0)
 selectedTile = (0, 0)
 
 (offset_x, offset_y) = (0, 0)
+mouse_x, mouse_y = 0, 0
 
 firstMove = True
 
@@ -43,7 +44,7 @@ def extendField(field, charToFind):
         for element in field[0]:
             border.append('')
         field.insert(0, border)
-        gameFieldAnchor = (gameFieldAnchor[0] - tileSize, gameFieldAnchor[1])
+        gameFieldAnchor = (gameFieldAnchor[0], gameFieldAnchor[1] - tileSize)
 
     # Check lower border
     if charToFind in field[-1]:
@@ -61,7 +62,7 @@ def extendField(field, charToFind):
     if not clearLeft:
         for i, _ in enumerate(field):
             field[i].insert(0, '')
-        gameFieldAnchor = (gameFieldAnchor[0], gameFieldAnchor[1] - tileSize)
+        gameFieldAnchor = (gameFieldAnchor[0] - tileSize, gameFieldAnchor[1])
 
     # Check right border
     clearRight = True
@@ -73,19 +74,32 @@ def extendField(field, charToFind):
         for i, _ in enumerate(field):
             field[i].append('')
 
-def whichTile(pos):
+def whichTile(pos, inclusive = False):
     for j, row in enumerate(gameField):
         for i, tile in enumerate(row):
-                if tile != '' \
-                  and pos[0] >= gameFieldAnchor[0] + offset_x + i * tileSize \
-                  and pos[0] <= gameFieldAnchor[0] + offset_x + (i + 1) * tileSize \
-                  and pos[1] >= gameFieldAnchor[1] + offset_y + j * tileSize \
-                  and pos[1] <= gameFieldAnchor[1] + offset_y + (j + 1) * tileSize:
-                    return (i, j)
+                if tile != '' or inclusive:
+                    if pos[0] >= gameFieldAnchor[0] + offset_x + i * tileSize \
+                      and pos[0] <= gameFieldAnchor[0] + offset_x + (i + 1) * tileSize \
+                      and pos[1] >= gameFieldAnchor[1] + offset_y + j * tileSize \
+                      and pos[1] <= gameFieldAnchor[1] + offset_y + (j + 1) * tileSize:
+                        return (i, j)
     return False
 
 def isMousePositionValid(pos):
-    return True
+    if whichTile(pos) is not False:
+        return False
+    if whichTile((pos[0], pos[1] - tileSize)) is not False \
+      or whichTile((pos[0], pos[1] + tileSize )) is not False \
+      or whichTile((pos[0] - tileSize, pos[1] - tileSize)) is not False \
+      or whichTile((pos[0] - tileSize, pos[1] + tileSize )) is not False \
+      or whichTile((pos[0] + tileSize, pos[1] - tileSize)) is not False \
+      or whichTile((pos[0] + tileSize, pos[1] + tileSize )) is not False \
+      or whichTile((pos[0] - tileSize, pos[1])) is not False \
+      or whichTile((pos[0] + tileSize, pos[1])) is not False:
+
+        return True
+
+    return False
 
 # Debug code
 extendField(gameField, playerChar)
@@ -107,17 +121,27 @@ while running:
                 pygame.quit()
             if event.key == pygame.K_SPACE:
                 gameFieldAnchor = (window_width / 2 - tileSize, window_height / 2 - tileSize)
+            if event.key == pygame.K_q:
+                print("valid tile:", whichTile((mouse_x, mouse_y), True))
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_x, mouse_y = event.pos
 
             if event.button == 1:
+
                 if firstMove:
                     firstMove = False
                     gameFieldAnchor = (event.pos[0] // tileSize * tileSize - tileSize, event.pos[1] // tileSize * tileSize - tileSize)
                     print('event.pos:', event.pos)
                     print('gameFieldAnchor', gameFieldAnchor)
                     gameField[1][1] = playerChar
+
+                if isMousePositionValid(event.pos):
+                    print("valid tile:", whichTile(event.pos, True))
+                    (x, y) = whichTile(event.pos, True)
+                    print(gameField)
+                    gameField[y][x] = playerChar
+                    print(gameField)
+                    extendField(gameField, playerChar)
 
 
             if event.button == 3:
@@ -126,6 +150,8 @@ while running:
 
         if event.type == pygame.MOUSEMOTION:
             # print(u'pressed buttons {}, position {{}{}} and relative movement {}'.format(event.buttons, event.pos, event.rel))
+
+            mouse_x, mouse_y = event.pos
 
             if event.buttons[2] == 1:
                 (tmp_x, tmp_y) = event.pos
@@ -156,7 +182,7 @@ while running:
     for j, row in enumerate(gameField):
         for i, tile in enumerate(row):
             if tile == playerChar:
-                pygame.draw.rect(screen, playerColor, [gameFieldAnchor[0] + offset_x + i * tileSize, gameFieldAnchor[1] + offset_y + i * tileSize, tileSize, tileSize])
+                pygame.draw.rect(screen, playerColor, [gameFieldAnchor[0] + offset_x + i * tileSize, gameFieldAnchor[1] + offset_y + j * tileSize, tileSize, tileSize])
 
 
     pygame.draw.circle(screen, (255, 0, 0), (int(gameFieldAnchor[0] + offset_x), int(gameFieldAnchor[1]) + offset_y), 5)
